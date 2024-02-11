be12a75226b9de32a9218d1d0216f4771f12e4df
Lucien Porta <lucien.porta@outlook.com>
2024-02-11 15:32:12 +0100

    main.py: add sace after each function Activate coreloss on stator

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Class import object3d, conductor, magnet, material, phase_winding
 app = Maxwell3d(projectname="MasterThesisExample",  solution_type="Transient", non_graphical=False)
 app.modeler.model_units = "meter"
 app.rename_design("2p6s_0334Hz", save_after_duplicate=True)
+
 #%% generate 2d geometries
 def createSurface(object3d) :
     polyline_list = []
@@ -22,8 +23,10 @@ def createSurface(object3d) :
 
 for obj in object3d.obj_inst :
     createSurface(obj)
+
 # %% substract 2d geometries
 app.modeler.subtract(app.modeler.get_object_from_name('stator'), app.modeler.get_object_from_name('stator_int'), keep_originals=False)
+
 # %% generate 3d geometries
 def createBody(object3d):
     if object3d.coordinate3d != None:
@@ -34,6 +37,7 @@ def createBody(object3d):
 
 for obj in object3d.obj_inst :
     createBody(obj)   
+
 # %% create materials
 def createMaterial(material):
     mat = app.materials.add_material(material.name)
@@ -48,6 +52,7 @@ def createMaterial(material):
 
 for mat in material.mat_inst:
     createMaterial(mat)
+
 # %% assign material to objects
 def assignMaterial(object3d):
     if object3d.material != None:
@@ -56,12 +61,14 @@ def assignMaterial(object3d):
 
 for obj in object3d.obj_inst:
     assignMaterial(obj)
+
 # %% create the phases and assign currents
 def createPhaseWinding(phase_winding):
     app.assign_winding(coil_terminals=None, winding_type="Current", is_solid=False, current_value=phase_winding.current, parallel_branches=1, name=phase_winding.name)
 
 for ph in phase_winding.ph_inst:
     createPhaseWinding(ph)
+
 #%% create the coil's teminals to assign current
 def createCoilTerminal(conductor):
     obj = app.modeler.get_object_from_name(conductor.name)
@@ -74,12 +81,14 @@ def createCoilTerminal(conductor):
 
 for cond in conductor.cond_inst:
     createCoilTerminal(cond)
+
 #%% assign the coils to one of the phase
 def assignCoilToPhase(conductor):
     app.add_winding_coils(windingname=conductor.phase_name, coil_names=[conductor.name+"_bottom", conductor.name+"_top"])
 
 for cond in conductor.cond_inst:
     assignCoilToPhase(cond)
+
 # %% set magnetization on the magnets
 def assignMagnetization(magnet):
     obj = app.modeler.get_object_from_name(magnet.name)
@@ -89,7 +98,8 @@ def assignMagnetization(magnet):
 
 for mag in magnet.mag_inst:
     assignMagnetization(mag)
-#%% create the model's outer region
+
+#%% create the model's outer region and assign boundary conditions
 app.modeler.create_cylinder(cs_axis="Z", position=[0,0,-0.027], radius="0.0252", height = "0.094", numSides="0", name="Region", matname="vacuum")
 
 shaft = app.modeler.get_object_from_name('shaft')
@@ -97,6 +107,7 @@ shaft_faces = [shaft.top_face_z, shaft.bottom_face_z]
 app.assign_insulating(shaft_faces)
 
 app.assign_rotate_motion(band_object=app.modeler.get_object_from_name('moving_out'), coordinate_system='Global', axis='Z', positive_movement=True, start_position='0deg', angular_velocity='20040')
+
 #%% split model to keep 1 pole only
 model_objects = app.modeler.object_list
 app.modeler.split(model_objects, plane="YZ", sides="PositiveOnly")
@@ -107,14 +118,15 @@ master_face = app.modeler.get_object_faces(obj_splited[0])[0]
 slave_face = app.modeler.get_object_faces(obj_splited[1])[0]
 app.assign_master_slave(master_entity=master_face, slave_entity=slave_face, u_vector_origin_coordinates_master=['0', '0', '0'], u_vector_pos_coordinates_master=['0', '0.1', '0'], u_vector_origin_coordinates_slave=['0', '0', '0'], u_vector_pos_coordinates_slave=['0', '-0.1', '0'], reverse_master=True, reverse_slave=True, same_as_master=False)
 app.change_symmetry_multiplier("2")
+
 #%% mesh settings
 app.mesh.assign_initial_mesh_from_slider(level=7, method="Auto", usedynamicsurface=True, useflexmesh=False, applycurvilinear=True, usefallback=True, usephi=False, automodelresolution=True)
 app.mesh.assign_length_mesh(['stator'], isinside=True, maxlength=0.005, maxel=None, meshop_name="stator")
-#app.mesh.assign_length_mesh(['shaft'], isinside=True, maxlength=0.005, maxel=None, meshop_name="shaft")
 app.mesh.assign_length_mesh(['magnet_1', 'magnet_2'], isinside=True, maxlength=0.005, maxel=None, meshop_name="magnet")
-#app.mesh.assign_length_mesh(['Region'], isinside=True, maxlength=0.01, maxel=None, meshop_name="region")
+
 #%% setup settings
 app.change_inductance_computation(compute_transient_inductance=True, incremental_matrix=False)
+app.set_core_losses(['stator'], value=True)
 setup = app.create_setup(setupname="0334Hz")
 setup.props["StopTime"] = "0.002944111776447106s"
 setup.props["TimeStep"] = "4.990019960079841e-05s"
@@ -129,10 +141,12 @@ setup.props["ScalarPotential"] = "Second Order"
 setup.props["SmoothBHCurve"] = False
 setup.update()
 pyaedt.settings.enable_pandas_output=True
+
 #%% validate, save and solve the setup
 app.validate_simple()
 app.save_project()
 app.analyze_setup("0334Hz")
+
 #%% export results and plot torque
 app.export_mesh_stats("0334Hz")
 result=app.post.get_solution_data("Moving1.Torque")
